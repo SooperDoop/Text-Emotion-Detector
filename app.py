fce1dbf4e1a9d8cb7c406dd11b8596a8d80acaa9
Shiva santosh <34417532+SooperDoop@users.noreply.github.com>
2019-10-25 19:39:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ def index():
 def index1():
     data = request.form['text']
     data1 = loaded_obj.predict_proba([data])
-    data1 = data1[0]
+    data1 = list(data1[0])
     data1[data1.index(max(data1))] +=0.5
-    return render_template('index.html',text1=data,ang=data1[0][0]*100,dis=data1[0][1]*100,fea=data1[0][2]*100,
-                           gui=data1[0][3]*100,joy=data1[0][5]*100,sad=data1[0][6]*100,sha=data1[0][7]*100)
+    return render_template('index.html',text1=data,ang=data1[0]*100,dis=data1[1]*100,fea=data1[2]*100,
+                           gui=data1[3]*100,joy=data1[5]*100,sad=data1[6]*100,sha=data1[7]*100)
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000)
